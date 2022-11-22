9bb5d201abdf5972a9d883e4c25a31d65efa90ce
hahaha <17611030607@163.com>
2022-11-22 19:01:45 +0800

    改成在命令提示行中输入用户名和密码

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,23 @@ package org.example;
 
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 
+import java.io.Console;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Util util = new Util();
+        Console console = System.console();
+        System.out.println("请输入用户名：");
+        String username = console.readLine();
+        System.out.println("请输入密码(不显示)：");
+        String password = new String(console.readPassword());
         while(true){
-            util.isConnection();
+            util.isConnection(username, password);
             try {
-                Thread.sleep(3600000); //每一小时测试一次
-                //Thread.sleep(100);
+                // 每一小时测试一次
+                Thread.sleep(3600000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -12,7 +12,7 @@ import java.util.Date;
 
 public class Util {
 
-    public boolean isConnection(){
+    public boolean isConnection(String username, String password){
         try {
             URL testurl = new URL("https://www.baidu.com/");
             testurl.openConnection().connect();
@@ -20,26 +20,26 @@ public class Util {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            //未连接网络 https://lgn.bjut.edu.cn/
+            // 未连接网络,访问https://lgn.bjut.edu.cn/
             System.out.println(new Date()+" -> 网络连接异常，进行重新连接");
             WebClient webClient = new WebClient();
             try {
                 HtmlPage htmlPage = webClient.getPage("https://lgn.bjut.edu.cn/");
                 HtmlForm htmlForm = htmlPage.getFormByName("form1");
-                //输入用户名
+                // 输入用户名
                 HtmlTextInput usernameInput = htmlForm.getInputByName("DDDDD");
-                usernameInput.setValueAttribute("");
-                //输入密码
+                usernameInput.setValueAttribute(username);
+                // 输入密码
                 HtmlPasswordInput passwordInput = htmlForm.getInputByName("upass");
-                passwordInput.setValueAttribute("");
-                //点击登录
+                passwordInput.setValueAttribute(password);
+                // 点击登录
                 HtmlSubmitInput htmlSubmitInput = htmlForm.getInputByName("0MKKey");
                 htmlSubmitInput.click();
                 System.out.println("网络连接成功");
             } catch (IOException ioException) {
                 ioException.printStackTrace();
             }catch (ElementNotFoundException elementNotFoundException){
-                //可能已经自动登录成功，再次访问会找不到标签元素
+                // 可能已经自动登录成功，再次访问会找不到标签元素
                 elementNotFoundException.printStackTrace();
             }
         }
